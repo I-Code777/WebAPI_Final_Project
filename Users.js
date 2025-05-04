500d9b4075492613601b0c7717e5615b60d423ff
I-Code777 <191649398+I-Code777@users.noreply.github.com>
2025-05-03 18:04:27 -0600

    added users.js and edited server.js

---
+++ Users.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Hash the password before saving
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Compare password for login
+UserSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
