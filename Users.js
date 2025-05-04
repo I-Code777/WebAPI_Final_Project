2483ab08160ae38110ba0ab1937f3b32f2eddbb3
I-Code777 <191649398+I-Code777@users.noreply.github.com>
2025-05-04 11:56:58 -0600

    Update Users.js

--- Users.js
+++ Users.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: { type: String, unique: true },
+  password: String,
+  // other fields
 });
 
-// Hash the password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-
-  try {
+// Hash the password before saving the user
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
   }
+  next();
 });
 
-// Compare password for login
-UserSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+// Compare password method
+userSchema.methods.comparePassword = async function(password) {
+  return bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+// Optional: Hash password method (if you use it for updates)
+userSchema.methods.hashPassword = async function(password) {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+module.exports = mongoose.model('User', userSchema);
